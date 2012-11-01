8adfd084976a2892ec4ec054e7b7fe1e380789fc
U-endor\chewie <chewie@endor.stanford.edu>
2012-10-31 22:51:00 -0700

    initial greasemonkey script

---
+++ vfsfw.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name          VikeFans SFW Script
+// @namespace     http://www.vikefans.com/sfw
+// @description   Turns the vikefans.com / guildportal site into a safe fore work site.
+// @include       http://www.guildportal.com/Guild.aspx?GuildID=440128*
+// @include       http://vikefans.com/*
+// @version       1.0
+// ==/UserScript==
